f383a959e06374f631c1f4439378f694417d0e28
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-03-26 12:17:19 +0200

    feature(course-info): add content for CourseInfo component

--- src/components/CourseInfo/CourseInfo.jsx
+++ src/components/CourseInfo/CourseInfo.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { IoCreate } from 'react-icons/io5';
+import { HiUsers } from 'react-icons/hi';
+import { FaHashtag } from 'react-icons/fa';
+import { MdAccessTimeFilled, MdArrowBackIos } from 'react-icons/md';
 
 import { Container } from '../../common/Container';
+import { Icon } from '../../common/Icon';
 import { useCourses } from '../../contexts/CoursesContext';
 import { useAuthors } from '../../contexts/AuthorsContext';
 import pipeDuration from '../../helpers/pipeDuration';
-import { CARD_TITLES, DURATION_UNITS } from '../../constants';
+import { BACK_BTN, DURATION_UNITS, ROUTES } from '../../constants';
 
-import { CourseInfoStyled } from './CourseInfo.styled';
-import { CourseDetails } from '../CreateCourse/CreateCourse.styled';
-import { CourseDetailsTitle } from '../Courses/components/CourseCard/CourseCard.styled';
+import {
+	CourseDetails,
+	CourseDetailsWrap,
+	CourseInfoContent,
+	CourseInfoStyled,
+	CourseInfoWrap,
+	CourseTitle,
+	LinkStyled,
+} from './CourseInfo.styled';
 
 const CourseInfo = () => {
 	const { courseId } = useParams();
@@ -23,32 +34,37 @@ const CourseInfo = () => {
 	return (
 		<CourseInfoStyled>
 			<Container>
-				<div>
-					<h1>{course.title}</h1>
-					<div>
+				<CourseInfoWrap>
+					<LinkStyled
+						path={ROUTES.COURSES}
+						text={BACK_BTN.text}
+						icon={<Icon component={MdArrowBackIos} />}
+					/>
+					<CourseTitle>{course.title}</CourseTitle>
+					<CourseInfoContent>
 						<p>{course.description}</p>
-						<CourseDetails>
-							<CourseDetailsTitle>{CARD_TITLES.ID}</CourseDetailsTitle>
-							<span>{course.id}</span>
-							<CourseDetailsTitle>{CARD_TITLES.DURATION}</CourseDetailsTitle>
-							<span>
-								{pipeDuration(course.duration)} {DURATION_UNITS}
-							</span>
-							<CourseDetailsTitle>{CARD_TITLES.CREATED}</CourseDetailsTitle>
-							<span>{course.creationDate.replaceAll('/', '.')}</span>
-							<CourseDetailsTitle>
-								{CARD_TITLES.COURSE_AUTHORS}
-							</CourseDetailsTitle>
-							<ul>
-								{courseAuthors.map((item) => (
-									<li key={item.id}>
-										<span>{item.name}</span>
-									</li>
-								))}
-							</ul>
-						</CourseDetails>
-					</div>
-				</div>
+						<CourseDetailsWrap>
+							<CourseDetails>
+								<Icon component={FaHashtag} />
+								<span>{course.id}</span>
+								<Icon component={MdAccessTimeFilled} />
+								<span>
+									{pipeDuration(course.duration)} {DURATION_UNITS}
+								</span>
+								<Icon component={IoCreate} />
+								<span>{course.creationDate.replaceAll('/', '.')}</span>
+								<Icon component={HiUsers} />
+								<ul>
+									{courseAuthors.map((item) => (
+										<li key={item.id}>
+											<span>{item.name}</span>
+										</li>
+									))}
+								</ul>
+							</CourseDetails>
+						</CourseDetailsWrap>
+					</CourseInfoContent>
+				</CourseInfoWrap>
 			</Container>
 		</CourseInfoStyled>
 	);
--- src/components/CourseInfo/CourseInfo.styled.js
+++ src/components/CourseInfo/CourseInfo.styled.js
@@ -1,5 +1,100 @@
 import styled from 'styled-components';
 
+import { Link } from '../../common/Link';
+
 export const CourseInfoStyled = styled.section`
 	padding: ${({ theme }) => theme.spacing[2]} 0;
+`;
+
+export const CourseInfoWrap = styled.div`
+	padding: ${({ theme }) => theme.spacing[3]};
+	background-color: ${({ theme }) => theme.colors.background.card};
+	box-shadow: ${({ theme }) => theme.shadows[0]};
+	border-radius: ${({ theme }) => theme.shape.borderRadius.m};
+	display: flex;
+	flex-direction: column;
+	gap: ${({ theme }) => theme.spacing[2]};
+	position: relative;
+`;
+
+export const CourseTitle = styled.h1`
+	padding: 0 ${({ theme }) => theme.spacing[5]};
+	font-weight: ${({ theme }) => theme.typography.weight.bold};
+	font-size: ${({ theme }) => theme.typography.size.xxxl};
+	position: relative;
+	margin-bottom: ${({ theme }) => theme.spacing[4]};
+
+	&::after {
+		content: '';
+		position: absolute;
+		top: calc(
+			100% + ${({ theme }) => theme.spacing[3]} -
+				${({ theme }) => theme.spacing[1]}
+		);
+		left: -${({ theme }) => theme.spacing[3]};
+		height: ${({ theme }) => theme.spacing[1]};
+		width: 95%;
+		border-top-right-radius: ${({ theme }) => theme.shape.borderRadius.s};
+		border-bottom-right-radius: ${({ theme }) => theme.shape.borderRadius.s};
+		background-color: ${({ theme }) => theme.colors.background.button};
+		background-image: linear-gradient(
+			-90deg,
+			${({ theme }) => theme.colors.white},
+			${({ theme }) => theme.colors.accent}
+		);
+
+		@media (min-width: ${({ theme }) => theme.breakpoints.tablet[0]}) {
+			width: 90%;
+		}
+
+		@media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+			text-align: center;
+		}
+	}
+`;
+
+export const CourseInfoContent = styled.div`
+	@media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+		display: flex;
+		flex-direction: column;
+		gap: ${({ theme }) => theme.spacing[3]};
+	}
+
+	@media (min-width: ${({ theme }) => theme.breakpoints.tablet[0]}) {
+		display: grid;
+		grid-template-columns: 3fr 2fr;
+		grid-gap: ${({ theme }) => theme.spacing[3]};
+	}
+
+	@media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+		grid-gap: ${({ theme }) => theme.spacing[4]};
+	}
+`;
+
+export const CourseDetailsWrap = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: ${({ theme }) => theme.spacing[3]};
+
+	@media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
+		gap: ${({ theme }) => theme.spacing[2]};
+	}
+`;
+
+export const CourseDetails = styled.div`
+	display: grid;
+	grid-template-columns: ${({ theme }) => theme.spacing[3]} auto;
+	gap: ${({ theme }) => theme.spacing[2]};
+
+	@media (min-width: ${({ theme }) => theme.breakpoints.tablet[0]}) {
+		align-self: flex-start;
+	}
+`;
+
+export const LinkStyled = styled(Link)`
+	position: absolute;
+	top: ${({ theme }) => theme.spacing[2]};
+	left: ${({ theme }) => theme.spacing[2]};
+	font-size: ${({ theme }) => theme.typography.size.s};
 `;
