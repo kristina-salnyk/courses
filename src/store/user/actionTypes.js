5a3822f41cab124d41f3f0220ad67e268d5ef759
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-03-28 12:27:41 +0200

    feature(actions): add user action types

---
+++ src/store/user/actionTypes.js
@@ -0,0 +1,7 @@
+export const USER_LOGIN = 'user/login';
+
+export const USER_LOGOUT = 'user/logout';
+
+export const USER_UPDATE = 'user/update';
+
+export const PERSIST_REHYDRATE = 'persist/REHYDRATE';
