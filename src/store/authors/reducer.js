358a59e2731fde130a6cd81d534b4660c171bfc3
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-03-29 22:09:40 +0200

    feature(redux): add authors reducer

--- src/store/authors/reducer.js
+++ src/store/authors/reducer.js
@@ -1,9 +1,11 @@
+import * as actionTypes from '../authors/actionTypes';
+
 const authorsReducer = (state = [], action) => {
 	switch (action.type) {
-		case 'authors/addAuthor':
-			return [...state, action.payload];
-		case 'authors/setAuthors':
-			return [...state, ...action.payload];
+		case actionTypes.AUTHORS_ADD:
+			return [action.payload, ...state];
+		case actionTypes.AUTHORS_FETCH:
+			return [...action.payload];
 		default:
 			return state;
 	}
--- src/store/authors/selectors.js
+++ src/store/authors/selectors.js
@@ -1,1 +1,22 @@
+import { createSelector } from 'reselect';
+
 export const selectAuthors = (state) => state.authors;
+
+export const selectAuthorsWithoutSelected = createSelector(
+	[selectAuthors, (state, selectedAuthors) => selectedAuthors],
+	(authors, selectedAuthors) =>
+		authors.filter((item) => !selectedAuthors.includes(item.id))
+);
+
+export const selectAuthorsByIds = createSelector(
+	[selectAuthors, (state, authorIds) => authorIds],
+	(authors, authorIds) => authors.filter((item) => authorIds.includes(item.id))
+);
+
+export const selectAuthorsListByIds = createSelector(
+	[selectAuthors, (state, authorIds) => authorIds],
+	(authors, authorIds) =>
+		authorIds
+			.map((item) => authors.find((author) => author.id === item)?.name)
+			.join(', ')
+);
