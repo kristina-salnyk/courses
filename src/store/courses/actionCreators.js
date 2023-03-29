44ae5cbddb79ca6800bb5be4b5b618346a2e615a
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-03-29 10:46:05 +0200

    feature(redux): add actions for courses slice

---
+++ src/store/courses/actionCreators.js
@@ -0,0 +1,39 @@
+import { v4 as uuid } from 'uuid';
+
+import * as actionTypes from './actionTypes';
+import dateGenerator from '../../helpers/dateGenerator';
+
+export const addCourse = ({ title, description, duration, authors }) => {
+	return {
+		type: actionTypes.COURSES_ADD,
+		payload: {
+			id: uuid(),
+			title,
+			description,
+			authors,
+			duration,
+			creationDate: dateGenerator(),
+		},
+	};
+};
+
+export const deleteCourse = (id) => {
+	return {
+		type: actionTypes.COURSES_DELETE,
+		payload: { id },
+	};
+};
+
+export const updateCourse = (id, course) => {
+	return {
+		type: actionTypes.COURSES_UPDATE,
+		payload: { ...course, id },
+	};
+};
+
+export const setCourses = (courses) => {
+	return {
+		type: actionTypes.COURSES_FETCH,
+		payload: [...courses],
+	};
+};
