278fd60fa128c839dcb2d8ce3dcf7a48d9bfd566
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-04-14 19:16:20 +0200

    feature(reducer-tests): add changes to courses reducer tests

--- src/store/tests/reducer.test.js
+++ src/store/tests/reducer.test.js
@@ -1,23 +1,19 @@
-import { addCourseAction, setCoursesAction } from '../courses/actionCreators';
 import reducer from '../courses/reducer';
+import { addCourseAction, setCoursesAction } from '../courses/actionCreators';
 import { MOCKED_COURSES_LIST } from '../../constants';
 
 test('should return the initial state', () => {
 	expect(reducer(undefined, { type: undefined })).toEqual([]);
 });
 
-test('should handle a course being added and returns new state', () => {
-	const prevState = [];
-
-	expect(
-		reducer(prevState, addCourseAction({ ...MOCKED_COURSES_LIST[0] }))
-	).toEqual([{ ...MOCKED_COURSES_LIST[0] }]);
+test('should handle COURSES_ADD and returns new state', () => {
+	expect(reducer([], addCourseAction({ ...MOCKED_COURSES_LIST[0] }))).toEqual([
+		{ ...MOCKED_COURSES_LIST[0] },
+	]);
 });
 
-test('should handle a courses being gotten and returns new state', () => {
-	const prevState = [];
-
-	expect(
-		reducer(prevState, setCoursesAction([...MOCKED_COURSES_LIST]))
-	).toEqual([...MOCKED_COURSES_LIST]);
+test('should handle COURSES_FETCH and returns new state', () => {
+	expect(reducer([], setCoursesAction([...MOCKED_COURSES_LIST]))).toEqual([
+		...MOCKED_COURSES_LIST,
+	]);
 });
