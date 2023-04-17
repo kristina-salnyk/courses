9868acbc38cd3de71ff1cc39b2789849319910ec
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-04-17 13:30:44 +0200

    feature(header-tests): add changes to Header component tests

--- src/components/Header/tests/header.test.js
+++ src/components/Header/tests/header.test.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { screen } from '@testing-library/react';
 
 import { Header } from '../index';
 import renderWithProviders from '../../../utils/renderWithProviders';
 import { LOGO_ALTERNATIVE_TEXT, MOCKED_STATE } from '../../../constants';
 
 test('should display logo', () => {
-	renderWithProviders(<Header />, { initialState: MOCKED_STATE });
+	const { getByAltText } = renderWithProviders(<Header />);
 
-	expect(screen.getByAltText(LOGO_ALTERNATIVE_TEXT)).toBeInTheDocument();
+	expect(getByAltText(LOGO_ALTERNATIVE_TEXT)).toBeInTheDocument();
 });
 
 test("should display user's name", () => {
-	renderWithProviders(<Header />, { initialState: MOCKED_STATE });
+	const { getByText } = renderWithProviders(<Header />);
 
-	expect(screen.getByText(MOCKED_STATE.user.name)).toBeInTheDocument();
+	expect(getByText(MOCKED_STATE.user.name)).toBeInTheDocument();
 });
